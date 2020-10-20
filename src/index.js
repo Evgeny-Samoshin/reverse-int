7286396b0bdceecb88e7f288f4d973ea62cfa7c6
Evgeny Samoshin <samevgeny@gmail.com>
2020-10-20 13:47:06 +0400

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
+
 module.exports = function reverse (n) {
-  
+    let arr = n.toString().split('');
+  while (arr[arr.length-1] === '0') {
+    arr.pop();
+  }
+  arr.reverse();
+  if (arr[arr.length-1] === '-') {
+    arr.pop();
+  }
+  return +arr.join('')
 }
